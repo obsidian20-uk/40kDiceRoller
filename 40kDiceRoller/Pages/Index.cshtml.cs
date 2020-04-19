e5c247426912b3b9a66d82ab3d84876c8927b889
obsidian81 <paul@simmonsonline.me.uk>
2020-04-19 09:33:37 +0100

    Initial version

---
+++ 40kDiceRoller/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace _40kDiceRoller.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public bool AdvancedWoundMode { get; set; }
+        public int NumHitDice { get; set; }
+
+        public int HitScore { get; set; }
+
+        public int WoundScore { get; set; }
+
+        public int SaveScore { get; set; }
+
+        public int TotalHits { get; set; }
+
+        public Dictionary<int, int> Results { get; set; } = new Dictionary<int, int>();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Results.Add(1, 2);
+            Results.Add(2, 3);
+            Results.Add(3, 3);
+            Results.Add(4, 2);
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
